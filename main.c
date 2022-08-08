87768fa9d0c8b3a3c7c64260a30723edbe877e77
TommsaoLabieni <259650@studenti.unimore.it>
2022-08-08 23:00:30 +0200

    Creation of the 3 CAN MSGS + first write logic for sending frames every 10Hz

--- main.c
+++ main.c
@@ -1,53 +1,88 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-#include <net/if.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-
-#include <linux/can.h>
-#include <linux/can/raw.h>
+#include <data_logger.h>
 
 int main(int argc, char **argv)
 {
-	int s; 
+	/* ********** VARS DECLARATION ********** */
+
+	__s32 s;
 	struct sockaddr_can addr;
 	struct ifreq ifr;
-	struct can_frame frame;
+	err_t ret;
+	error_msg error;
+	__u32 nbytes;
+	can_msg msg_500, msg_501, msg_502;
 
-	printf("CAN Sockets Demo\r\n");
+	/* ************************************** */
 
-	if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
-		perror("Socket");
-		return 1;
+
+	/* ********** CONNECTION TO CAN NETWORK ********** */
+
+	/* Socket Creation */
+	s = socket(PF_CAN, SOCK_DGRAM, CAN_BCM);
+
+	/* Connection to CAN with interface = DEVICE_INTERFACE */
+	strcpy(ifr.ifr_name, DEVICE_INTERFACE);
+	ret = ioctl(s, SIOCGIFINDEX, &ifr);
+	
+	if (ret)
+	{
+		sprintf(error, "Error during connection to CAN Bus interface: %s. Error code is: %d\n", DEVICE_INTERFACE, ret);
+		perror(error);
+		exit(1);
 	}
 
-	strcpy(ifr.ifr_name, "vcan0" );
-	ioctl(s, SIOCGIFINDEX, &ifr);
-
-	memset(&addr, 0, sizeof(addr));
 	addr.can_family = AF_CAN;
 	addr.can_ifindex = ifr.ifr_ifindex;
 
-	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-		perror("Bind");
-		return 1;
+	/* Open Socket Connection */
+	ret = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+
+	printf("Connected to: %s\n", DEVICE_INTERFACE);
+
+	if (ret)
+	{
+		sprintf(error, "Error during socket connection. Error code is %d\n", ret);
+		perror(error);
+		exit(2);
 	}
 
-	frame.can_id = 0x555;
-	frame.can_dlc = 5;
-	sprintf(frame.data, "Hello");
+	printf("Socket connection created!\n");
 
-	if (write(s, &frame, sizeof(struct can_frame)) != sizeof(struct can_frame)) {
-		perror("Write");
-		return 1;
+	/* *********************************************** */
+
+	/* ********** CAN FRAMES AND FILTERS CREATION ********** */
+
+	/* CAN Frames Creation */
+	create_frame(&msg_500, 0x500, MSG_500_DLC);
+	create_frame(&msg_501, 0x501, MSG_501_DLC);
+	create_frame(&msg_502, 0x502, MSG_502_DLC);
+
+	/* Fixed frames creation (of course to be removed; this is just an example on how to write to the can frame) */
+    memcpy(msg_500.frame.data,(__u8[]){0x00,0x28,0xFF,0x00,0x00,0x01,0xFF,0x00},MSG_500_DLC);
+    memcpy(msg_501.frame.data,(__u8[]){0x00,0x32,0xFF,0x00,0x00,0x01},MSG_501_DLC);
+    memcpy(msg_502.frame.data,(__u8[]){0x00,0x3C,0xFF,0x00,0x00},MSG_502_DLC);
+	
+	//TODO: Add Filters
+
+	/* ***************************************************** */
+
+	/* Write CAN messages to kernel space. From now on, the kernel will transmit all 3 CAN frames to the CAN network every 100ms without the need of the syscall write() */
+	write(s, &msg_500, sizeof(can_msg));
+	write(s, &msg_501, sizeof(can_msg));
+	write(s, &msg_502, sizeof(can_msg));
+
+	while(1) {;}
+
+	/* Session terminated -> Close socket connection. */
+	ret = close(s);
+	if (ret)
+	{
+		sprintf(error, "Error during socket close. Error code is: %d\n", ret);
+		perror(error);
+		exit(3);
 	}
 
-	if (close(s) < 0) {
-		perror("Close");
-		return 1;
-	}
+	printf("Socket connection closed.\n\nTerminating\n");
+
 	return 0;
 }
