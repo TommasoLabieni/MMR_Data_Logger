a369b6bb558137258bd670fd60c81cdfed2fb2d6
TommsaoLabieni <259650@studenti.unimore.it>
2022-08-10 01:29:54 +0200

    CAN frame MASK creation considering message PBS and PBE

--- include/data_logger.h
+++ include/data_logger.h
@@ -25,5 +25,7 @@ typedef struct {
 void create_egress_BCM(can_msg* msg, canid_t can_id, __u8 dlc);
 void create_sniffer_filters(struct can_filter* filters, const cJSON_msg_50x_t* msg);
 char* get_MMR_CAN_frame_description(cJSON_msg_50x_t* msg_50x, canid_t msg_id);
+unsigned int get_MMR_CAN_frame_info_index(cJSON_msg_50x_t* msg_50x, canid_t msg_id);
 canid_t MMR_CAN_frame_to_EGRESS_frame(cJSON_msg_50x_t* msg_50x, canid_t msg_id);
+int insert_MMR_CAN_frame_into_EGRESS_frame(struct can_frame recv_frame, cJSON_msg_50x_t* msg_50x);
 #endif //DATA_LOGGER_H
